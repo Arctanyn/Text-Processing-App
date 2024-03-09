aef58f5466efd9e1f0423c61e8c22d0c335577a2
Malil Dugulubgov <81229461+Arctanyn@users.noreply.github.com>
2024-03-09 15:27:34 +0300

    Initial Commit

---
+++ Text-Processing/Text_ProcessingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Text_ProcessingApp.swift
+//  Text-Processing
+//
+//  Created by Malil Dugulubgov on 09.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct Text_ProcessingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
